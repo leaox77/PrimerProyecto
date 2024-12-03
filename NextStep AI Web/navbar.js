46962a9b3ee65bc8e86f81e1044cd31dd5a71690
leaox77 <leandroeguerdo@gmail.com>
2024-12-03 05:59:06 -0400

    navbar-responsive arreglado

---
+++ NextStep AI Web/navbar.js
@@ -0,0 +1,7 @@
+const menuToggle = document.getElementById('menu-toggle');
+const nav = document.getElementById('nav');
+
+menuToggle.addEventListener('click', () => {
+    
+  nav.classList.toggle('active');
+});
